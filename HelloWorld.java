cff43072319f91391ef1ca0bc87954d7371650ae
DewsDavid31 <33354172+DewsDavid31@users.noreply.github.com>
2017-12-28 07:20:45 -0700

    HelloWorld.java
    Simple Hello World program in Java with java-docs will do the trick!

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+
+/**
+@Method HelloWorld  Simple program designed to print "Hello World!" into a system's output.
+@Return             Void
+@Args               None
+@Author             David Dews
+*/
+  public static void main(String [] args){
+
+    System.out.println("Hello World!"); // Println is used
+
+  }
+}
